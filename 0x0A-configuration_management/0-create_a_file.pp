2cdc1d68a9922a85632272a14485080d5e87dd4f
kirlos basta <kirlosbasta@gmail.com>
2024-03-22 23:11:15 +0200

    Add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in tmp
+file {'/tmp/school':
+    ensure  => present,
+    content => 'I love Puppet',
+    group   => 'www-data',
+    owner   => 'www-data',
+    mode    => '0744',
+}
