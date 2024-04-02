89257e1b3b09e15c70b42f69b7ec2303daa6527a
kirlos basta <kirlosbasta@gmail.com>
2024-04-02 04:29:05 +0200

    Add 1-install_load_balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,18 +14,6 @@ package { 'nginx':
     ensure   => installed,
 }
 
-file { 'html':
-    ensure  => present,
-    path    => '/var/www/html/index.nginx-debian.html',
-    content => 'Hello World!',
-}
-
-file_line { 'configure redirection':
-    path  =>  '/etc/nginx/sites-available/default',
-    after =>  'server_name _;',
-    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtube/dQw4w9WgXcQ;\n\t}\n"
-}
-
 file_line { 'http_header':
     ensure => present,
     path   => '/etc/nginx/sites-available/default',
