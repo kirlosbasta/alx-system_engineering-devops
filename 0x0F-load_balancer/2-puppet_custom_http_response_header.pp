73a0a264aa9021ea92c1ead21d69add3a22d9415
kirlos basta <kirlosbasta@gmail.com>
2024-04-03 01:30:18 +0200

    Add 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,7 +12,7 @@ file_line { 'http_header':
     ensure => present,
     path   => '/etc/nginx/sites-available/default',
     after  => 'server_name _;',
-    line   => "\tadd_header X-Served-By \"${hostname}\";",
+    line   => "\tadd_header X-Served-By ${hostname};",
     notify => Exec['restart_nginx'],
 }
 
