ce4d187e112acc3c6424f01f7ee249143e9e8067
kirlos basta <kirlosbasta@gmail.com>
2024-04-03 01:34:25 +0200

    Add 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,16 @@
 #  install and configure an Nginx server
-exec { 'apt_update':
-    command => '/usr/bin/apt-get -y update',
-    path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-    ensure   => installed,
+-> package { 'nginx':
+  ensure => installed,
 }
-
-file_line { 'http_header':
-    ensure => present,
-    path   => '/etc/nginx/sites-available/default',
-    after  => 'server_name _;',
-    line   => "\tadd_header X-Served-By ${hostname};",
-    notify => Exec['restart_nginx'],
+-> file_line { 'header_served_by':
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => "\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
 }
-
-service { 'nginx':
-    ensure => running,
-    enable => true,
-    notify => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-    command => '/usr/sbin/service nginx restart',
-    path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+-> exec {'restart':
+  command => '/usr/sbin/service nginx restart',
 }
