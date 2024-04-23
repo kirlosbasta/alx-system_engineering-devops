e57aa399376cb80d72006b8de65c378398ec84c0
kirlos basta <kirlosbasta@gmail.com>
2024-04-23 04:39:30 +0200

    Add 0-gather_data_from_an_api.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''''''
+import json
+import requests
+import sys
+
+
+def main():
+    '''Entry point to the module'''
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    user_url = 'https://jsonplaceholder.typicode.com/users/'
+    id = sys.argv[1]
+    todo_responce = requests.get(todo_url, params={'userId': id})
+    user_responce = requests.get(user_url + id)
+    user_json = json.loads(user_responce.text)
+    todo_json = json.loads(todo_responce.text)
+    user_name = user_json["name"]
+    total_number_of_tasks = len(todo_json)
+    number_of_done_tasks = 0
+    for i in range(total_number_of_tasks):
+        if todo_json[i]['completed']:
+            number_of_done_tasks += 1
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user_name, number_of_done_tasks, total_number_of_tasks
+        ))
+    for i in range(total_number_of_tasks):
+        if todo_json[i]['completed']:
+            print('\t ' + todo_json[i]['title'])
+
+
+if __name__ == '__main__':
+    main()
