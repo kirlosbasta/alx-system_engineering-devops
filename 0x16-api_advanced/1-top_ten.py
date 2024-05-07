ca993548e0656bfbfba27bf10b172c95c51c95e7
kirlos basta <kirlosbasta@gmail.com>
2024-05-08 02:39:53 +0300

    Add 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,10 @@ def top_ten(subreddit):
     '''Return the top 10 hot post for a given subreddit'''
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Myscript/2.0 (by /u/kirlos)'}
-    data = {'limit': 9}
-    response = requests.get(url, headers=headers, params=data)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        for post in response.json().get('data').get('children'):
-            print(post.get('data').get('title'))
+        data = response.json().get('data').get('children')
+        for i in range(10):
+            print(data[i].get('data').get('title'))
     else:
         print(None)
