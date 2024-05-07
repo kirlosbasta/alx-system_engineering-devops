2923862b48e948dd3c0f298fb09ca21fdf48ce26
kirlos basta <kirlosbasta@gmail.com>
2024-05-08 01:48:47 +0300

    Add 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Mozilla/5.0'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     return 0
